6fa3a24e9d6230a44e33a87b2e6c4292ed1d7bab
Thomas P <60588709+th-ms@users.noreply.github.com>
2023-04-07 11:00:26 -0500

    Moved products to products.js

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -1,50 +1,10 @@
 import React, { useState } from "react";
-
-const products = [
-    {
-        title: "Sun Glasses",
-        price: 100.0,
-        description: "Lorem ipsum.",
-        image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f",
-    },
-    {
-        title: "Shoes",
-        price: 60.0,
-        description: "Fresh.",
-        image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    },
-    {
-        title: "Watch",
-        price: 80.0,
-        description: "Simple.",
-        image: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
-    },
-    {
-        title: "Headphones",
-        price: 50.0,
-        description: "Noise-cancelling.",
-        image: "https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=984&q=80",
-    },
-    {
-        title: "Backpack",
-        price: 45.0,
-        description: "Water-resistant.",
-        image: "https://images.unsplash.com/photo-1514524865930-188150490d83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
-    },
-    {
-        title: "Camera",
-        price: 250.0,
-        description: "DSLR.",
-        image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-];
+import products from "./products";
 
 const App = () => {
     const [quantities, setQuantities] = useState(
         Array(products.length).fill(0)
     );
-
-
 
 // Add an array of US states
 const usStates = [
